3d8347c13b452050a8050e5101d5a5aba56f410c
Joshua Martinez <joshkmartinez@gmail.com>
2019-08-15 10:25:20 -0700

    Removed Website

--- index.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res) => {
-  res.writeHead(302, {
-    Location:
-      "https://discordapp.com/api/oauth2/authorize?client_id=592968118905733120&permissions=0&scope=bot"
-  });
-  res.end();
-};
